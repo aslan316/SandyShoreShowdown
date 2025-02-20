10a188ad45e7d1ebb11844c2e45dedd33e29d211
aslan316 <ljhord31@gmail.com>
2025-02-20 13:14:54 -0500

    Create initial particle system

--- src/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc *.c -L ../raylib/src/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -I ../raylib/src/  -o gameTest
--- src/test.c
+++ src/test.c
@@ -1,41 +1,133 @@
 #include "raylib.h"
+#define MAX_PARTICLES 50
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 960
 
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+typedef enum{
+	ENEMY=0,
+	POWER_UP_HEALTH=1,
+	POWER_UP_INVIS=2,
+	POWER_UP_DOUBLE_FIRE_DAMAGE=4,
+	POWER_UP_DOUBLE_ENEMY_DAMAGE=8,
+	POWER_UP_SPREAD=16,
+	BOX=32
+} ParticleType;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+typedef struct {
+	int x;
+	int y;
+	int dy;
+	int dx;
+	Color color;
+	int w;
+	int h;
+	int health;
+	ParticleType type;
+	bool isAlive;
+}Particle;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+typedef struct{
+	Particle particles[MAX_PARTICLES];
+	int speed;
+	int count;
+}ParticleSystem;
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+ParticleSystem* particle_system_init(int speed);
+void particle_system_free(ParticleSystem* system);
+void particle_draw(Particle* particle);
+void particle_draw_system(ParticleSystem* system);
+void particle_update_system(ParticleSystem* system);
+void particle_update(Particle* particle);
+void particle_create(Particle* particle);
+void particle_system_create_particle(ParticleSystem* system);
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+int main(void){
 
-            ClearBackground(RAYWHITE);
+	InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "Sandy Shore Tech Demo 1");
+	SetTargetFPS(60);
+	ParticleSystem* system=particle_system_init(20);
+	
+	while(!WindowShouldClose()){
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		if(GetRandomValue(0,100) > 80){
+			particle_system_create_particle(system);
+		}
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
+		particle_update_system(system);	
+		BeginDrawing();
+	
+			ClearBackground(RAYWHITE);
+			particle_draw_system(system);
+		EndDrawing();
+	}
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+	CloseWindow();
+	particle_system_free(system);
+	return 0;
+}
 
-    return 0;
+ParticleSystem* particle_system_init(int speed){
+	ParticleSystem* system = MemAlloc(sizeof(ParticleSystem));
+	system->speed=speed;
+	system->count=0;
+	for(int i=0; i < MAX_PARTICLES; i++){
+		system->particles[i].isAlive = false;
+	}
+	return system;
+}
+
+void particle_system_free(ParticleSystem* system){
+	MemFree(system);
+}
+
+void particle_draw(Particle* particle){
+	DrawRectangle(particle->x, particle->y, particle->w, particle->h, particle->color);
+}
+
+void particle_draw_system(ParticleSystem* system){
+	for(int i=0; i<MAX_PARTICLES; i++){
+		if(system->particles[i].isAlive){
+			particle_draw(&system->particles[i]);
+		}
+	}
+}
+
+void particle_update_system(ParticleSystem* system){
+	for(int i=0; i<MAX_PARTICLES; i++){
+		particle_update(&system->particles[i]);
+	}
+}
+
+void particle_update(Particle* particle){
+	if(particle->isAlive){
+		if(particle->y > SCREEN_HEIGHT + particle->h){
+			particle->isAlive = false;
+		}else{
+			particle->y += particle->dy;
+		}
+	}
+}
+
+void particle_create(Particle* particle){
+	particle->x = GetRandomValue(0, SCREEN_WIDTH);
+	particle->y =0;
+	particle->dy=1;
+	particle->w=32;
+	particle->h=32;
+	particle->type = (ParticleType)GetRandomValue(0, 6);
+  particle->isAlive = true;
+	if(particle->type != ENEMY){
+		particle->type = (ParticleType)(particle->type | BOX);
+		particle->color = GREEN;
+	}else{
+		particle->color = RED;
+	}
+}
+void particle_system_create_particle(ParticleSystem* system){
+	for(int i=0; i < MAX_PARTICLES; i++){
+		if(!system->particles[i].isAlive){
+			particle_create(&system->particles[i]);
+			return;
+		}
+	}
 }
