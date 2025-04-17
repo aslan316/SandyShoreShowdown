58c89cea43e32fec548a5700a5e468838689e6be
aslan316 <ljhord31@gmail.com>
2025-04-17 14:10:26 -0400

    Implement basic character movement

---
+++ build/CMakeFiles/gameTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gameTest.dir/link.d"
+  "CMakeFiles/gameTest.dir/src/test.c.o"
+  "CMakeFiles/gameTest.dir/src/test.c.o.d"
+  "gameTest"
+  "gameTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gameTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
